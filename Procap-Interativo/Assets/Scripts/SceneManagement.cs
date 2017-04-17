bdabe7cd9284bf4c2051b71ec6b19354145b56f3
Gabriel Balarin <balaringabriel@gmail.com>
2017-04-17 17:22:27 -0300

    SceneManager script & Prof Scene created

---
+++ Procap-Interativo/Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+	public void SouAluno ()
+	{
+		SceneManager.LoadScene ("Aluno");
+	}
+
+	public void SouProfessor ()
+	{
+		SceneManager.LoadScene ("Professor");
+	}
+
+	public void Return ()
+	{
+		SceneManager.LoadScene ("SelecaoAlunoProfessor");
+	}
+}
